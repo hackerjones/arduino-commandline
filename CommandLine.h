3532ddc3222376b95aa75e446f959d133476229f
Mark Jones <markalanj@gmail.com>
2012-04-05 23:33:22 -0500

    updated header guard

--- CommandLine.h
+++ CommandLine.h
@@ -17,8 +17,8 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
-#ifndef _CONSOLE_H_
-#define _CONSOLE_H_
+#ifndef _COMMANDLINE_H_
+#define _COMMANDLINE_H_
 
 #include <Arduino.h>
 
